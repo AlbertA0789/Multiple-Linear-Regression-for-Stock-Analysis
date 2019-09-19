dffed6a753f2f0dba3e6cc76583fd376e5d0076f
AlbertA0789 <luckyalbert185@gmail.com>
2019-09-19 16:13:36 +0700

    Add files via upload

--- Multiple-Linear-Regression-for-Stock-Analysis/Multiple-Linear-Regression-for-Stock-Analysis/DataPreprocessing.py
+++ Multiple-Linear-Regression-for-Stock-Analysis/Multiple-Linear-Regression-for-Stock-Analysis/DataPreprocessing.py
@@ -1,12 +1,25 @@
 import pandas as pd
 
+def fileVerification( filePath ):
+    while not( filePath.endswith(('.csv','.xls','.xlsx','.sql'))):
+        print('File can only be Comma Seperated Value, Excel or Sql')
+        filePath = input(' Please Enter the path of the file that is valid : ')
+    else:
+        print('File accepted. Please wait for a moment while the file is  being processed.')
+        return filePath
+        
+
 def findSuitableReadMethod( filePath ):
     if filePath.endswith('.csv'):
         readResult = pd.read_csv( filePath )
     elif filePath.endswith('.xls') or filePath.endswith('.xlsx'):
         readResult = pd.read_excel( filePath )
+    elif filePath.endswith('.sql'):
+        readResult = pd.read_sql( filePath )
     return readResult
 
 if __name__ == '__main__':
-    readResult = findSuitableReadMethod('D:\Chrome Download\iris_data.csv')
+    inputFilePath = input(' Enter the path of the file : ')
+    inputFilePath = fileVerification( inputFilePath )
+    readResult = findSuitableReadMethod( inputFilePath )
     print( readResult )
